377a7d7e6e3a02cfb9de30b72cea83ac0afe03f0
Kyse7535 <ktevot@gmail.com>
2025-06-17 08:20:19 +0000

    it works

--- index.js
+++ index.js
@@ -27,16 +27,36 @@ class Course {
 }
 
 class Course_manager {
-    constructor() {
-        this.courses = []
-        this.course_en_cours = null;
+    constructor(pause_manager) {
+        this.courses = JSON.parse(localStorage.getItem("courses")) || []
+        this.course_en_cours = JSON.parse(localStorage.getItem("course_en_cours")) ?
+            Object.assign(new Course(), JSON.parse(localStorage.getItem("course_en_cours"))) : null;
         this.montant = document.getElementById("montant");
+        this.btn_start_course = document.getElementById("start-course")
+        this.btn_stop_course = document.getElementById("stop-course")
+        //retirer les boutons pauses
+        //affiche les boutons de pause
+        this.pause_manager = pause_manager;
+
+    }
+
+    init() {
+        //SI j'ai une course en cours j'affiche le stop course button...
+        if (this.course_en_cours && !this.course_en_cours.heure_arrivee) {
+            this.btn_stop_course.style.display = "block"
+            this.btn_start_course.style.display = "none"
+
+            document.getElementById("stop-day").style.display = "none"
+            document.getElementById("pause-day").style.display = "none"
+            document.getElementById("end-pause-day").style.display = "none"
+
+            this.montant.value = this.course_en_cours.montant
+        }
     }
 
     start(controller) {
-        const btn_start_course = document.getElementById("start-course")
-        const btn_stop_course = document.getElementById("stop-course")
-        btn_start_course.addEventListener("click", e => {
+        let that = this
+        this.btn_start_course.addEventListener("click", e => {
             if (this.montant.value === 0 || this.montant.value === "") {
                 alert("Merci de saisir le montant avant de lancer une course")
                 return;
@@ -47,20 +67,50 @@ class Course_manager {
             }
             this.creer_course(this.courses.length + 1);
             alert("course demarré");
+
+            //retirer les boutons pauses et de fin de journée pendant la course
+            const btn_pause_day = document.getElementById("pause-day")
+            const btn_fin_pause_day = document.getElementById("end-pause-day")
+            btn_fin_pause_day.style.display = "none"
+            btn_pause_day.style.display = "none"
+
+            //affiche le btn fin de course et retire le btn start course
+            that.btn_start_course.style.display = "none"
+            that.btn_stop_course.style.display = "block"
+
+            //retire le btn fin de journée
+            document.getElementById("stop-day").style.display = "none"
             controller.stop();
             e.preventDefault();
         })
-        btn_stop_course.addEventListener("click", e => {
+        this.btn_stop_course.addEventListener("click", e => {
             if (this.course_en_cours === null) {
                 alert("Aucune course en cours");
                 return;
             }
             //termine la course et l'enregistre
-            this.course_en_cours.terminer()
-            this.courses.push(this.course_en_cours);
-            this.course_en_cours = null;
-            console.log("courses: ", this.courses)
+            if (that.course_en_cours !== null) {
+                that.course_en_cours.terminer()
+                that.montant.value = 0
+            }
+            that.courses.push({ ...this.course_en_cours });
+            that.course_en_cours = null;
             console.log("total: ", this.calculer_total())
+
+            that.btn_start_course.style.display = "block"
+            that.btn_stop_course.style.display = "none"
+
+            //affiche les boutons de pause
+            const btn_pause_day = document.getElementById("pause-day")
+            const btn_fin_pause_day = document.getElementById("end-pause-day")
+            btn_pause_day.style.display = "block"
+
+            //affiche le btn fin de journée
+            document.getElementById("stop-day").style.display = "block"
+
+
+            localStorage.setItem("courses", JSON.stringify(that.courses))
+            localStorage.removeItem("course_en_cours")
 
             //start controller
             controller.start()
@@ -69,7 +119,8 @@ class Course_manager {
     }
 
     creer_course(id) {
-        this.course_en_cours = new Course(id, this.montant);
+        this.course_en_cours = new Course(id, this.montant.value);
+        localStorage.setItem("course_en_cours", JSON.stringify({ ...this.course_en_cours }))
     }
 
     calculer_total() {
@@ -182,32 +233,207 @@ class Recorder {
     }
 }
 
+class Arret {
+    constructor() {
+        this.heure_debut = new Date().toLocaleString("fr-FR", { timeZone: "UTC" });
+        this.heure_fin = null
+    }
+
+    setHeureDebut(heure) {
+        this.heure_debut = heure
+    }
+
+    setHeureFin(heure) {
+        this.heure_fin = heure
+    }
+}
+
+class Day {
+    constructor() {
+        this.heure_debut = null
+        this.arrets = []
+        this.heure_fin = null
+        this.arret_en_cours = null
+        this.status = "NOT_STARTED"
+    }
+}
+
+
+class TabDisplay {
+    constructor(parameters) {
+        this.btn_stop_day = document.getElementById("stop-day")
+        this.btn_start_day = document.getElementById("start-day")
+        this.identifiant = document.getElementById("identifiant")
+    }
+    hide_tab() {
+        localStorage.removeItem("identifiant")
+        document.getElementById("tab").style.display = "block"
+        this.btn_stop_day.style.display = "none"
+        this.btn_start_day.style.display = "block"
+        this.identifiant.style.display = "block"
+    }
+    display_tab() {
+
+    }
+}
+
 class DayManager {
-    constructor(controller) {
-        const btn_start_day = document.getElementById("start-day")
-        const btn_stop_day = document.getElementById("stop-day")
-        btn_start_day.addEventListener("click", e => {
-            const identifiant = document.getElementById("identifiant")
-            if (identifiant.value === "") {
-                alert("Merci de saisir votre identifiant")
+    constructor(controller, course_manager) {
+        this.day = Object.assign(new Day(), JSON.parse(localStorage.getItem("day"))) || new Day()
+        let that = this
+        this.start_day = this.start_day.bind(this)
+        this.controller = controller
+        this.course_manager = course_manager
+        this.btn_start_day = document.getElementById("start-day");
+        this.btn_stop_day = document.getElementById("stop-day");
+        this.btn_pause_day = document.getElementById("pause-day");
+        this.btn_fin_pause_day = document.getElementById("end-pause-day");
+        const identifiant = localStorage.getItem("identifiant");
+        if (this.day.status === "STARTED") {
+            this.display_btn()
+        }
+        this.btn_start_day.addEventListener("click", e => {
+            this.start_day(identifiant);
+            e.preventDefault();
+        })
+        this.btn_stop_day.addEventListener("click", e => {
+            const confirm = window.confirm("Cette action est irreversible. oulez-vous continuer ?")
+            if (!confirm) {
                 return;
             }
-            identifiant.style.display = "none";
-            btn_start_day.style.display = "none"
-            controller.start()
+            this.stop_day();
+            e.preventDefault();
+        })
+        this.btn_pause_day.addEventListener("click", e => {
+            that.day.status = "PAUSED";
+            that.day.arret_en_cours = new Arret();
+            localStorage.setItem("day", JSON.stringify(that.day));
+
+            //arreter le controller
+            that.controller.stop()
+
+            that.btn_fin_pause_day.style.display = "block"
+            that.btn_pause_day.style.display = "none"
+
+            //retire la possibilité de faire une courses pendant la pause
+            document.getElementById("montant").style.display = "none"
+            document.getElementById("start-course").style.display = "none"
+
+            //retire la possibilité de terminer la journée
+            document.getElementById("stop-day").style.display = "none"
+        })
+        this.btn_fin_pause_day.addEventListener("click", e => {
+            that.day.status = "STARTED"
+            const arret_en_cours = { ...that.day.arret_en_cours }
+            that.day.arret_en_cours = Object.assign(new Arret(), arret_en_cours)
+
+            if (that.day.arret_en_cours instanceof Arret) {
+                that.day.arret_en_cours.setHeureFin(new Date().toLocaleString("fr-FR", { timeZone: "UTC" }))
+            }
+            that.day.arrets.push({ ...that.day.arret_en_cours })
+            that.day.arret_en_cours = null
+            localStorage.setItem("day", JSON.stringify(that.day));
+
+            that.btn_pause_day.style.display = "block"
+
+            that.btn_fin_pause_day.style.display = "none"
+
+
+            //retablis la possibilité de faire une courses pendant la pause
+            document.getElementById("montant").style.display = "block"
+            document.getElementById("start-course").style.display = "block"
+
+            //retablis la possibilité de terminer la journée
+            document.getElementById("stop-day").style.display = "block"
+
+            that.controller.start()
+        })
+        this.detect_arret();
+    }
+
+    init() {
+        if (this.day && this.day.heure_debut && !this.day.heure_fin) {
             document.getElementById("tab").style.display = "block"
-            btn_stop_day.style.display = "block"
-            btn_start_day.style.display = "none"
-            e.preventDefault()
-        })
-        btn_stop_day.addEventListener("click", e => {
-            document.getElementById("tab").style.display = "none"
-            btn_start_day.style.display = "block"
-            identifiant.style.display = "block"
-            btn_stop_day.style.display = "none"
-            controller.stop()
-            e.preventDefault()
-        })
+
+            document.getElementById("identifiant").style.display = "none"
+            document.getElementById("start-day").style.display = "none"
+        }
+
+        if (this.day && this.day.arret_en_cours) {
+            this.btn_fin_pause_day.style.display = "block"
+            this.btn_pause_day.style.display = "none"
+            this.btn_stop_day.style.display = "none"
+
+            document.getElementById("montant").style.display = "none"
+            document.getElementById("start-course").style.display = "none"
+            document.getElementById("stop-course").style.display = "none"
+        }
+    }
+
+    display_btn() {
+        document.getElementById("tab").style.display = "block"
+        this.btn_stop_day.style.display = "block"
+        this.btn_start_day.style.display = "none"
+        document.getElementById("identifiant").style.display = "none"
+        this.btn_pause_day.style.display = "block"
+        document.getElementById("stop-course").style.display = "none"
+    }
+
+    hide_btn() {
+        localStorage.removeItem("identifiant")
+        document.getElementById("tab").style.display = "none"
+        this.btn_stop_day.style.display = "none"
+        this.btn_start_day.style.display = "block"
+        document.getElementById("identifiant").style.display = "block"
+    }
+
+    start_day(identifiant_saved) {
+        this.day.heure_debut = new Date().toLocaleString("fr-FR", { timeZone: "UTC" });
+        this.day.status = "STARTED"
+        localStorage.setItem("day", JSON.stringify(this.day))
+        if (!identifiant_saved && identifiant.value === "") {
+            alert("Merci de saisir votre identifiant")
+            return;
+        }
+        if (!identifiant_saved) {
+            localStorage.setItem("identifiant", identifiant.value)
+        }
+        this.display_btn()
+        this.controller.start()
+    }
+
+
+    stop_day() {
+        this.day.status = "ENDED"
+        this.day.heure_fin = new Date().toLocaleString("fr-FR", { timeZone: "UTC" });
+        localStorage.setItem("day", JSON.stringify(this.day))
+
+        this.hide_btn()
+        this.controller.stop()
+    }
+
+    detect_arret() {
+        let that = this
+        document.addEventListener("visibilitychange", (e) => {
+            //Si je sors de l'app alors que je suis en pleine activité
+            //J'enregistre un arret
+            that.day = Object.assign(new Day(), JSON.parse(localStorage.getItem("day")))
+            if (document.visibilityState === "hidden"
+                && this.day.status === "STARTED" && this.course_manager && !this.course_manager.course_en_cours) {
+                that.day.arret_en_cours = new Arret()
+                localStorage.setItem("day", JSON.stringify({ ...that.day }))
+                console.log("🕵️ L'utilisateur quitte ou minimise l'app");
+            }
+            if (document.visibilityState === 'visible'
+                && that.day.status === "STARTED" && that.day.arret_en_cours instanceof Arret) {
+
+                that.day.arret_en_cours.setHeureFin(new Date().toLocaleString("fr-FR", { timeZone: "UTC" }))
+                that.day.arrets.push({ ...that.day.arret_en_cours })
+                that.day.arret_en_cours = null;
+                localStorage.setItem("day", JSON.stringify({ ...that.day }))
+                console.log("👀 L'utilisateur est revenu dans l'app");
+            }
+        });
     }
 }
 
@@ -216,6 +442,7 @@ class IntervalController {
         this.callback = callback; // Fonction à exécuter
         this.delay = delay;       // Délai en ms
         this.intervalId = null;
+
     }
 
     start() {
@@ -238,6 +465,7 @@ class IntervalController {
     isRunning() {
         return this.intervalId !== null;
     }
+
 }
 
 
@@ -247,12 +475,14 @@ window.addEventListener("load", e => {
             .then((reg) => console.log("service worker registred", reg))
             .catch((err) => console.log("service worker not registered", err))
     }
+
     const cm = new Course_manager();
     const recorder = new Recorder();
     recorder.start()
     const controller = new IntervalController(recorder.captureSilentPhoto, 10000)
     cm.start(controller)
-    const btn_analyser = document.getElementById("analyser")
 
-    const day_manager = new DayManager(controller);
+    const day_manager = new DayManager(controller, cm);
+    cm.init()
+    day_manager.init()
 })
